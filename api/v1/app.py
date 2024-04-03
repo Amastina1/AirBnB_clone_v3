3be8d62a5457a4ab7bae8dda7702f3dc33cf4f38
paschalugwu <ugwupaschal@gmail.com>
2024-04-03 18:04:31 +0100

    feat(api): Add API status endpoint
    Created the initial structure for the API, including the status endpoint at /api/v1/status.
    
    Created api folder at the project root with an empty __init__.py file.
    Added v1 folder inside api with an empty __init__.py.
    Implemented app.py within v1 folder:
    Defined Flask instance app.
    Imported storage from models.
    Imported app_views from api.v1.views.
    Registered the blueprint app_views to the Flask instance app.
    Defined a method to handle @app.teardown_appcontext that calls storage.close().
    Ran the Flask server with host and port from environment variables or default values.
    Created views folder inside v1 with an empty __init__.py.
    Implemented index.py within views folder:
    Imported app_views from api.v1.views.
    Defined a route /status on the app_views object that returns a JSON response with the status OK.
    GitHub repository: AirBnB_clone_v3
    
    File changes:
    
    api/init.py
    api/v1/init.py
    api/v1/app.py
    api/v1/views/init.py
    api/v1/views/index.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,42 +1,41 @@
 #!/usr/bin/python3
-"""Flask API to return status"""
-
+""" Flask api to return status
+"""
+from api.v1.views import app_views
 from flask import Flask, jsonify, make_response
 from flask_cors import CORS
 from models import storage
 from os import getenv
-from api.v1.views import app_views
 
-# Create a Flask application instance
 app = Flask(__name__)
-
-# Enable CORS (Cross-Origin Resource Sharing)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+app.register_blueprint(app_views)
 CORS(app, origins='0.0.0.0')
 
-# Register the blueprint for API endpoints
-app.register_blueprint(app_views)
 
-# Define a method to handle teardown of the Flask application context
 @app.teardown_appcontext
 def teardown(exception):
-    """Destroys DB session in case of DB storage,
-    reloads objects in case of File Storage"""
+    """ destroys DB session in case of DB storage
+        reloads objects in case of File Storage
+    """
     storage.close()
 
-# Define an error handler for 404 Not Found errors
+
 @app.errorhandler(404)
 def error_404(error):
-    """Response for 404 errors"""
+    """ response for 404 errors"""
     return make_response(jsonify({"error": "Not found"}), 404)
 
 
-# Run the Flask application
 if __name__ == "__main__":
-    # Get host address from environ variable HBNB_API_HOST/default to '0.0.0.0'
-    host = getenv('HBNB_API_HOST') or '0.0.0.0'
+    #  get host address
+    host = getenv('HBNB_API_HOST')
+    if host is None:
+        host = '0.0.0.0'
 
-    # Get port number from environment variable HBNB_API_PORT/default to 5000
-    port = int(getenv('HBNB_API_PORT') or 5000)
+    #  get port number
+    port = getenv('HBNB_API_PORT')
+    if port is None:
+        port = '5000'
 
-    # Start the Flask server
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=host, port=port)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-""" views functionality """
+""" views functionality
+"""
 from api.v1.views import app_views
+from flask import jsonify
+from models import storage
 from flask import jsonify, make_response
 from models import storage
 from models.amenity import Amenity
@@ -10,22 +13,18 @@ from models.review import Review
 from models.state import State
 from models.user import User
 
-# Define a route /status on the app_views Blueprint
+
 @app_views.route('/status', methods=['GET'])
 def status():
-    """Return status OK on route /status"""
+    """ return status OK on route /status"""
     return jsonify({"status": "OK"})
 
-# Define a route /stats on the app_views Blueprint
+
 @app_views.route('/stats', methods=['GET'])
 def stats():
-    """Return count of all objects per class"""
-    # Define classes and corresponding models
+    """ returns count of all objects per class"""
     classes = {"amenities": Amenity, "cities": City, "places": Place,
                "reviews": Review, "states": State, "users": User}
 
-    # Count objects for each class using storage.count()
     obj_count = {key: storage.count(value) for key, value in classes.items()}
-
-    # Return the object count as a JSON response
     return make_response(jsonify(obj_count))

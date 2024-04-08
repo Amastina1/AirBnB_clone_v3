7ef6cec7eca32c9a410c6b74bc694ded89b087fd
paschalugwu <ugwupaschal@gmail.com>
2024-04-08 11:57:23 +0100

    feat(api): Add endpoint to retrieve object stats
    Create a new endpoint in api/v1/views/index.py to retrieve the number of each object by type using the newly added count() method from storage.
    
    Define route /api/v1/stats
    Implement logic to retrieve object counts for amenities, cities, places, reviews, states, and users
    GitHub repository: AirBnB_clone_v3
    Affected file: api/v1/views/index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,12 @@
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status')
@@ -14,12 +20,12 @@ def status():
 
 @app_views.route('/stats')
 def stats():
-    '''returns number of each objects by type'''
+    '''returns the count of all objects by type'''
     return jsonify({
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User")
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
     })

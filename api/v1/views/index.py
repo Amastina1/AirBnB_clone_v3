76635c61a40f9bf47aa57931f2ca3a210907f90e
paschalugwu <ugwupaschal@gmail.com>
2024-04-08 11:12:17 +0100

    feat(api): Add endpoint to retrieve object stats
    Create a new endpoint in api/v1/views/index.py to retrieve the number of each object by type using the newly added count() method from storage.
    
    Define route /api/v1/stats
    Implement logic to retrieve object counts for amenities, cities, places, reviews, states, and users
    GitHub repository: AirBnB_clone_v3
    Affected file: api/v1/views/index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -11,15 +11,15 @@ def status():
     '''returns json object with the app status'''
     return jsonify({"status": "OK"})
 
+
 @app_views.route('/stats')
-def get_stats():
-    '''returns the number of each object by type'''
-    stats = {
+def stats():
+    '''returns number of each objects by type'''
+    return jsonify({
         "amenities": storage.count("Amenity"),
         "cities": storage.count("City"),
         "places": storage.count("Place"),
         "reviews": storage.count("Review"),
         "states": storage.count("State"),
         "users": storage.count("User")
-    }
-    return jsonify(stats)
+    })
